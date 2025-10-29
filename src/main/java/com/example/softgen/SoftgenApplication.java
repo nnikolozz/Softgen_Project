f00bf49772aa4640c740f495aa8f1a75462c8e31
Nikoloz Gigauri <kochogori@gmail.com>
2025-10-29 15:58:14 +0400

    first commit

---
+++ src/main/java/com/example/softgen/SoftgenApplication.java
@@ -0,0 +1,13 @@
+package com.example.softgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftgenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftgenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/softgen/SoftgenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.softgen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftgenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
